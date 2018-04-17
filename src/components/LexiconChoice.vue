51e4bcd71712fb241bcaa11a52b9ce02a7486ecd
Maria Öhrman <maria.ohrman@gu.se>
2018-04-17 09:43:46 +0200

    lexicon choice style

--- src/components/LexiconChoice.vue
+++ src/components/LexiconChoice.vue
@@ -1,7 +1,7 @@
 <template>
   <b-modal id="lexiconModal" :title="loc('select_lexicon')" v-model="showLexiconModal">
     <ul>
-      <li v-for="lexicon in lexicons" @click="selectLexicon(lexicon.name)">{{lexicon.name}} ({{lexicon.open ? 'open' : 'closed'}})</li>
+      <li v-for="lexicon in lexicons" @click="selectLexicon(lexicon.name)"><span :style="getLexiconStyle(lexicon.name)">{{lexicon.name}}</span> ({{lexicon.open ? 'open' : 'closed'}})</li>
     </ul>
     <div slot="modal-footer"></div>
   </b-modal>
@@ -31,11 +31,20 @@ export default {
         this.update('lexicon', lexicon)
         this.showLexiconModal = false
       }
+    },
+    getLexiconStyle (lexicon) {
+      if(lexicon === this.globals.hot.lexicon) {
+        return {'font-weight': 'bold'}
+      } else {
+        return {}
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+ul {
+  list-style-type: none;
+}
 </style>
